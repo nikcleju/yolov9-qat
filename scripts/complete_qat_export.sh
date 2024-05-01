11502f54dee5a779ef7dec26ca5086499eab3604
Nicolae Cleju <ncleju@etti.tuiasi.ro>
2024-05-01 19:40:40 +0300

    Add batch size option

--- scripts/complete_qat_export.sh
+++ scripts/complete_qat_export.sh
@@ -14,12 +14,14 @@ set -e
 #IMGSZ=960
 BASEMODEL=$1
 IMGSZ=$2
-NAME=qat
+BATCHSIZE=${3:-10}
+QUANTIZENAME=quantize
+EVALNAME=eval
 # ---
 PTMODEL=$BASEMODEL-converted.pt
 PROJECT=runs/qat_$BASEMODEL_$IMGSZ
-BEST_WEIGHTS=$PROJECT/$NAME/weights/qat_best_$PTMODEL
+BEST_WEIGHTS=$PROJECT/$QUANTIZENAME/weights/qat_best_$PTMODEL
 # ---
-python3 qat.py quantize --weights $PTMODEL --imgsz $IMGSZ --project $PROJECT --name $NAME
-python3 qat.py eval --weights $BEST_WEIGHTS  --imgsz $IMGSZ --project $PROJECT --name $NAME
+python3 qat.py quantize --weights $PTMODEL --imgsz $IMGSZ --project $PROJECT --name $QUANTIZENAME --batch-size $BATCHSIZE
+python3 qat.py eval --weights $BEST_WEIGHTS  --imgsz $IMGSZ --project $PROJECT --name $EVALNAME
 python3 export_qat.py  --weights $BEST_WEIGHTS --include onnx_end2end
